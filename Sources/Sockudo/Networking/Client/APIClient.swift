026609f9e13cd4c213908d88389ce0aa111ae9be
Diaconu Radu-Mihai <52667211+countradooku@users.noreply.github.com>
2026-04-01 13:11:06 +0300

    fix: resolve SwiftLint line_length violation in APIClient.swift

--- Sources/Sockudo/Networking/Client/APIClient.swift
+++ Sources/Sockudo/Networking/Client/APIClient.swift
@@ -27,5 +27,7 @@ struct APIClient: APIotaClient {
 extension APIClient {
 
     /// The default Channels HTTP API headers that should be appended to each `URLRequest` made by the receiver.
-    static let defaultHeaders: HTTPHeaders = [HTTPHeader("X-Sockudo-Library"): "sockudo-http-swift \(SDKVersion.current)"]
+    static let defaultHeaders: HTTPHeaders = [
+        HTTPHeader("X-Sockudo-Library"): "sockudo-http-swift \(SDKVersion.current)"
+    ]
 }
